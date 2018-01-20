b5d70bd3b6c63123463b9b9c5c168d0c78e8ba07
ivancordeiro <ivancordeiro@yahoo.com.br>
2018-01-20 15:35:15 -0200

    'up'

--- www/db.js
+++ www/db.js
@@ -15,27 +15,9 @@ function onInit(){
            // onUpdateDB( 3, 'joao23', 'daa4s57ree23', 'cli' ); //id, nome, token, tipo 
             //onDeleteDB(2);//id
 
-       //var net = navigator.connection.type;
 
-//alert('a net e do tipo: ' + net );
-
-//if( net == 'Connection.NONE' ){
-//alert('O app não encontrou conexao com internet' );
-//} else {
-
-var conn = conexao();
-
-if( conn == 'none' || conn == 'NONE' ){
-conexaoMSG(conn);
-} else {
-checaLogin();
-}
-
-
-
-
-//}
-            
+             checaLogin();
+      
 
         }
     } 
