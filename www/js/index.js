3da30a998d59d4418bc3f054f5e1ac677911dc65
ivancordeiro <ivancordeiro@yahoo.com.br>
2018-01-20 02:29:45 -0200

    'up'

--- www/js/index.js
+++ www/js/index.js
@@ -18,11 +18,43 @@
  */
 document.addEventListener("deviceready", onDeviceReady, false); 
 
+
 function onDeviceReady() {
+
+modelo =  device.model;
+plataforma = device.platform;
+uuid = device.uuid;
+version =  device.version;
+serial = device.serial;
 
 //pedidos();
 
 }
+
+
+
+
+function checkConnection() {
+
+alert('testando conexao:');
+var networkState = navigator.connection.type;
+
+alert('networkState:' + networkState );
+
+var states = '';
+if( networkState == 'Connection.UNKNOWN' ){    states  = 'Unknown connection'; }
+if( networkState == 'Connection.ETHERNET' ){        states = 'Ethernet connection'; }
+if( networkState == 'Connection.WIFI' ){        states     = 'WiFi connection'; }
+if( networkState == 'Connection.CELL_2G' ){        states  = 'Cell 2G connection'; }
+if( networkState == 'Connection.CELL_3G' ){        states  = 'Cell 3G connection'; }
+ if( networkState == 'Connection.CELL_4G' ){       states  = 'Cell 4G connection'; }
+if( networkState == 'Connection.CELL' ){        states     = 'Cell generic connection'; }
+if( networkState == 'Connection.NONE' ){        states     = 'No network connection'; }
+
+    alert('Connection type: ' + states );
+}
+
+
 
 function pedidos(){
 alert('pedidos');
